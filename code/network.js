d8c41a3772b0762f405d6c98d5dbe64ea0f3bef6
Tahj Anderson <tahjanderson@115.188.21.198.tigernet.wifi.dyn.clemson.edu>
2022-11-15 09:37:49 -0500

    data and progress for network

--- code/network.js
+++ code/network.js
@@ -2,5 +2,55 @@ d3.json("/ProjectData/events_European_Championship.json").then(
     
     function(events_data){
         console.log(events_data)
+
+        var dimensions = {
+            width:300,
+            height: 150,
+            margin : {
+                top: 30
+            }
+        }
+        var back_x = dimensions.width/16
+        var mid_x = dimensions.width/2
+        var front_x = dimensions.width *  15/16
+        var svg = d3.select("#network")
+                    .style("width", dimensions.width)
+                    .style("height", dimensions.height)
+
+        var xScale = d3.scaleLinear()
+                       .domain([0,100])
+                       .range([0, dimensions.width])
+
+        var yScale = d3.scaleLinear()
+                       .domain([100,0])
+                       .range([dimensions.margin.top, dimensions.height])
+
+        var back_line = svg.append("g")
+                        .append('line')
+                        .attr('x1', back_x)
+                        .attr('y1', dimensions.height)
+                        .attr('x2', back_x)
+                        .attr('y2', 0)
+                        .style("stroke", "black")
+                        .style("stroke-width", "2")
+        var mid_line = svg.append("g")
+                          .append('line')
+                          .attr('x1', mid_x)
+                          .attr('y1', dimensions.height)
+                          .attr('x2', mid_x)
+                          .attr('y2', 0)
+                          .style("stroke", "black")
+                          .style("stroke-width", "2")
+        var front_line = svg.append("g")
+                            .append('line')
+                            .attr('x1', front_x)
+                            .attr('y1', dimensions.height)
+                            .attr('x2', front_x)
+                            .attr('y2', 0)
+                            .style("stroke", "black")
+                            .style("stroke-width", "2")
+        
+        var connections = svg.append("g")
+
     }
 )
--- data_engineering.py
+++ data_engineering.py
@@ -10,4 +10,15 @@ def cut_off_rows(file_path):
     df = df.loc[df["matchId"] == 1694390]
     print(df)
 
-cut_off_rows("events_data.csv")
+#cut_off_rows("events_data.csv")
+
+def eval_data(file_path):
+    df = pd.read_csv(file_path)
+    print(df)
+
+#convert_json_to_csv("ProjectData/players.json", "ProjectData/players.csv")
+
+def combine_rows(data1, data2):
+    df1 = pd.read_csv(data1)
+    df2 = pd.read_csv(data2)
+
